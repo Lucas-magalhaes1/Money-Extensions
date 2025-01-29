e9953e1e0fd186ad6fbdaafc855e306f65da838f
Lucas Magalhães <lucasmagalhaes728@gmail.com>
2025-01-28 22:18:52 -0300

    First version

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,15 @@
+﻿namespace MoneyExtension.Tests;
+
+[TestClass]
+public class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 277.89M;
+        var cents = valor.ToCents();
+        
+        Assert.AreEqual(27789, cents);
+    }
+}
+
